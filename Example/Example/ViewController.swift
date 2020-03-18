1ec5c3e153affc7c24f0f2e7426c966bac38efcb
trantuanan08121996 <trantuanan08121996@gmail.com>
2020-03-18 13:50:19 +0700

    first consent

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by antrantuan on 3/18/20.
+//  Copyright © 2020 d1intern. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
